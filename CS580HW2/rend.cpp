1e79f3fad32bdf35838e1a64ce2f2e33e128a11c
DaStar2B <DaStar2B@0edd4750-7f39-49f4-c00c-7378fc34be3f>
2010-09-14 14:49:14 +0000

    Implemented GzNewRender, GzFreeRender, and GzPutAttribute.

--- CS580HW2/rend.cpp
+++ CS580HW2/rend.cpp
@@ -13,6 +13,22 @@ int GzNewRender(GzRender **render, GzRenderClass renderClass, GzDisplay *display
 - span interpolator needs pointer to display for pixel writes
 - check for legal class GZ_Z_BUFFER_RENDER
 */
+	// check for bad pointers
+	if( !render || !display )
+		return GZ_FAILURE;
+
+	GzRender * tmpRenderer = new GzRender;
+	// make sure allocation worked
+	if( !tmpRenderer )
+		return GZ_FAILURE;
+
+	tmpRenderer->renderClass = renderClass;
+	tmpRenderer->display = display;
+	tmpRenderer->open = 0; // zero value indicates that renderer is closed
+
+	// the rest of the struct members will be initialized on begin render?
+
+	*render = tmpRenderer;
 
 	return GZ_SUCCESS;
 }
@@ -23,6 +39,15 @@ int GzFreeRender(GzRender *render)
 /* 
 -free all renderer resources
 */
+	// check for bad pointer
+	if( !render )
+		return GZ_FAILURE;
+
+	// do NOT free the display here. It should be freed elsewhere.
+	render->display = NULL;
+
+	free( render );
+	render = NULL;
 
 	return GZ_SUCCESS;
 }
@@ -43,7 +68,26 @@ int GzPutAttribute(GzRender	*render, int numAttributes, GzToken	*nameList,
 /*
 - set renderer attribute states (e.g.: GZ_RGB_COLOR default color)
 - later set shaders, interpolaters, texture maps, and lights
+TRANSLATION:
+Assign the values in valueList to render based on categories in nameList
+ - For this assignment, check for GZ_RGB_COLOR in nameList and assign the color values in valueList to render variable
 */
+	// check for bad pointers
+	if( !render || !nameList || !valueList )
+		return GZ_FAILURE;
+
+	for( int i = 0; i < numAttributes; i++ )
+	{
+		// check for GZ_RGB_COLOR in nameList
+		if( nameList[i] == GZ_RGB_COLOR )
+		{
+			GzColor * colorPtr = ( static_cast<GzColor *>( valueList[i] ) );
+			// assign the color values in valueList to render variable
+			render->flatcolor[RED] = ( *colorPtr )[RED];
+			render->flatcolor[GREEN] = ( *colorPtr )[GREEN];
+			render->flatcolor[BLUE] = ( *colorPtr )[BLUE];
+		}
+	}
 
 	return GZ_SUCCESS;
 }
@@ -58,6 +102,8 @@ int GzPutTriangle(GzRender *render, int	numParts, GzToken *nameList,
       GZ_NULL_TOKEN:		do nothing - no values
       GZ_POSITION:		3 vert positions in model space
 - Invoke the scan converter and return an error code
+TRANSLATION:
+Similar to GzPutAttribute but check for GZ_POSITION and GZ_NULL_TOKEN in nameList
 */
 
 	return GZ_SUCCESS;
