66fd6b1a755131fc262ed4934dce5d07fc5514a5
DaStar2B <DaStar2B@0edd4750-7f39-49f4-c00c-7378fc34be3f>
2010-10-11 03:21:58 +0000

    Fixed compliation error in code. In Assignment 4, GZ_FLAT (flat shading) does not exist.

--- CS580HW4/rend.cpp
+++ CS580HW4/rend.cpp
@@ -330,8 +330,8 @@ int GzNewRender(GzRender **render, GzRenderClass renderClass, GzDisplay	*display
 	// initialize default specular power
 	tmpRenderer->spec = DEFAULT_SPEC;
 
-	// use flat shading as the default interpolation mode
-	tmpRenderer->interp_mode = GZ_FLAT;
+	// use phong shading as the default interpolation mode
+	tmpRenderer->interp_mode = GZ_NORMALS;
 
 	// the rest of the struct members will be initialized in GzBeginRender.
 
