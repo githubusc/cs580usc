0ba914d7823854d7be90ed6ee5d023df2298bd70
DaStar2B <DaStar2B@0edd4750-7f39-49f4-c00c-7378fc34be3f>
2010-11-16 01:01:28 +0000

    Fixed alpha definition and added error checking for alpha. Also continued troubleshooting Gouraud texture shading issue so messed with some App5 values.

--- CS580HW5/Application5.cpp
+++ CS580HW5/Application5.cpp
@@ -176,10 +176,10 @@ GzMatrix	rotateY =
         valueListShader[4] = (GzPointer)&specpower;
 
         nameListShader[5]  = GZ_TEXTURE_MAP;
-#if 1   /* set up null texture function or valid pointer */
+#if 0   /* set up null texture function or valid pointer */
         valueListShader[5] = (GzPointer)0;
 #else
-        valueListShader[5] = (GzPointer)(ptex_fun);	/* or use ptex_fun */
+        valueListShader[5] = (GzPointer)(tex_fun);	/* or use ptex_fun */
 #endif
         status |= GzPutAttribute(m_pRender, 6, nameListShader, valueListShader);
 
